8315159fb51981b6255175078d6e6731054a0522
finalSay9 <chimwazaevan1@gmail.com>
2026-05-22 13:28:40 +0200

    generating the modules for each service auth,users,payment,loans,notification ans audit

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { LoansModule } from './loans/loans.module';
+import { PaymentsModule } from './payments/payments.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { AuditModule } from './audit/audit.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, LoansModule, PaymentsModule, NotificationsModule, AuditModule],
   controllers: [AppController],
   providers: [AppService],
 })
