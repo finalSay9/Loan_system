96c043de26cd47d2b0529273f8a13256c8bdc8c8
finalSay9 <chimwazaevan1@gmail.com>
2026-05-28 12:55:07 +0200

    the registration is up and working

--- api/src/users/dto/create-user.dto.ts
+++ api/src/users/dto/create-user.dto.ts
@@ -1,35 +1,43 @@
 import { Transform } from 'class-transformer';
-import { 
-  IsEmail, 
-  IsEnum, 
-  IsNotEmpty, 
-  IsOptional, 
-  IsString, 
-  Matches, 
-  MinLength 
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MinLength,
 } from 'class-validator';
-import { Role } from '../../../prisma/generated/prisma';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-
+import { Role } from '../../../prisma/generated/prisma';
 
 export class CreateUserDto {
-  @ApiProperty({ example: 'John Banda', description: 'Full name of the user' })
+  @ApiProperty({ example: 'peter Banda', description: 'Full name of the user' })
   @IsString()
   @IsNotEmpty()
-  @MinLength(2, { message: 'name must be at least 2 characters' })
+  @MinLength(2, { message: 'Name must be at least 2 characters' })
   @Transform(({ value }) => (typeof value === 'string' ? value.trim() : value))
   name!: string;
 
+  @ApiProperty({
+    example: 'Area 49, Lilongwe',
+    description: 'Physical address',
+  })
   @IsString()
   @IsNotEmpty()
   @Transform(({ value }) => (typeof value === 'string' ? value.trim() : value))
   address!: string;
 
+  @ApiProperty({ example: 'Software Engineer', description: 'Occupation' })
   @IsString()
   @IsNotEmpty()
   @Transform(({ value }) => (typeof value === 'string' ? value.trim() : value))
   occupation!: string;
 
+  @ApiProperty({
+    example: '+265991234567',
+    description: 'Phone in E.164 format',
+  })
   @IsString()
   @IsNotEmpty()
   @Matches(/^\+?[1-9]\d{1,14}$/, {
@@ -37,18 +45,36 @@ export class CreateUserDto {
   })
   phone!: string;
 
+  @ApiPropertyOptional({
+    example: 'john@example.com',
+    description: 'Email address',
+  })
   @IsEmail({}, { message: 'Invalid email address' })
   @IsOptional()
   @Transform(({ value }) =>
     typeof value === 'string' ? value.trim().toLowerCase() : value,
   )
-  email!: string;
+  email?: string;
 
+  @ApiProperty({
+    example: 'Secret@123',
+    description:
+      'Min 8 chars, must include upper, lower, number and special character',
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/, {
+    message:
+      'Password must contain uppercase, lowercase, number and special character',
+  })
   password!: string;
 
+  @ApiPropertyOptional({
+    enum: Role,
+    default: Role.BORROWER,
+    description: 'User role',
+  })
   @IsEnum(Role)
   @IsOptional()
   role?: Role = Role.BORROWER;
