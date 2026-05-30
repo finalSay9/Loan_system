d9a0879d63975f3a993367b6aaf0a486ef4d754d
finalSay9 <chimwazaevan1@gmail.com>
2026-05-30 12:48:20 +0200

    adding the api decorator to the fields

--- api/src/auth/dto/login.dto.ts
+++ api/src/auth/dto/login.dto.ts
@@ -1,7 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 
 export class LoginDto {
-    IsString()
+    @IsString()
     phone: string
+
+
+     @ApiProperty({
+        example: 'Secret@123',
+        description:
+          'Min 8 chars, must include upper, lower, number and special character',
+      })
+    @IsString()
+    password: string
 }
