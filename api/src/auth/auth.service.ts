8fbdda6dd0365951dcb409627306c808b54f3801
finalSay9 <chimwazaevan1@gmail.com>
2026-05-30 12:37:32 +0200

    creating the validateUser method in auth service so that it can be used in controllers correctly

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { LocalGuard } from './guards/local.guard';
+import { JwtStrategy, LocalStrategy } from './strategies/jwt.strategy';
+
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService){}
+
+    async validateUser(){}
     
 }
