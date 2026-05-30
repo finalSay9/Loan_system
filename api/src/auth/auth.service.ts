a54f9f930a84cb1d8f9d4133a1281a83f449e4e4
finalSay9 <chimwazaevan1@gmail.com>
2026-05-30 13:01:23 +0200

    adding the login logic to the auth service

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,16 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { LocalGuard } from './guards/local.guard';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 import { LoginDto } from './dto/login.dto';
-
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService){}
 
     async validateUser(dto: LoginDto){
+        const user = await this.prisma.user.findUnique({
+            where: {phone: dto.phone}
+        });
+        if(!user){
+            throw new UnauthorizedException('invalid credentials')
+        }
+        //check password
+        const checkPassword = await bcrypt.compare(dto.password, user.passwordHash)
+
+        if(!checkPassword) {
+            throw new UnauthorizedException('invalid credentials')
+        }
         
     }
     
