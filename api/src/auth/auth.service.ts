aafb6d5d49dacdaa9f0da88f67f4122d7b77f545
finalSay9 <chimwazaevan1@gmail.com>
2026-05-31 12:48:08 +0200

    changing to the normal way of implementing it without the use of the dto

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 import { LocalGuard } from './guards/local.guard';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
@@ -8,22 +9,34 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService){}
+    constructor(
+        private prisma: PrismaService,
+        private jwtService: JwtService
+    ){}
 
-    async validateUser(dto: LoginDto){
+    async validateUser(phone: string, password:string){
         const user = await this.prisma.user.findUnique({
-            where: {phone: dto.phone}
+            where: {phone: phone}
         });
         if(!user){
             throw new UnauthorizedException('invalid credentials')
         }
         //check password
-        const checkPassword = await bcrypt.compare(dto.password, user.passwordHash)
+        const checkPassword = await bcrypt.compare(password, user.passwordHash)
         //if long credentials provided
         if(!checkPassword) {
             throw new UnauthorizedException('invalid credentials')
         }
         
+    }
+
+    async login(user: LoginDto){
+        const payload = {phone: user.phone, sub: user.password};
+
+        return {
+            access_token: this.jwtService.sign(payload)
+        }
+
     }
     
 }
