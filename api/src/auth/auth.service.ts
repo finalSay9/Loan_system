0a63e4fb3d4f4db89122b04dba7fb074b2708cf3
finalSay9 <chimwazaevan1@gmail.com>
2026-06-08 11:29:50 +0200

    fixing the bugs in the login and validate user function

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -6,36 +6,37 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private prisma: PrismaService,
-        private jwtService: JwtService
-    ){}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
-    async validateUser(phone: string, password:string): Promise<any>{
-        const user = await this.prisma.user.findUnique({
-            where: {phone: phone}
-        });
-        if(!user){
-            throw new UnauthorizedException('invalid credentials')
-        }
-        //check password
-        const checkPassword = await bcrypt.compare(password, user.passwordHash)
-        //if wrong credentials provided
-        if(!checkPassword) {
-            throw new UnauthorizedException('invalid credentials')
-        }
-        return user;
-        
+  async validateUser(phone: string, password: string): Promise<any> {
+    const user = await this.prisma.user.findUnique({
+      where: { phone: phone },
+    });
+    if (!user) {
+      throw new UnauthorizedException('invalid credentials');
     }
-
-    async login(user: LoginDto){
-        const payload = {phone: user.phone, sub: user.password};
-
-        return {
-            message: 'login successfull',
-            access_token: this.jwtService.sign(payload)
-        }
-
+    //check password
+    const checkPassword = await bcrypt.compare(password, user.passwordHash);
+    //if wrong credentials provided
+    if (!checkPassword) {
+      throw new UnauthorizedException('invalid credentials');
     }
-    
+    // Strip out the password hash so it doesn't get passed around the app
+    const { passwordHash, ...result } = user;
+
+    return result;
+  }
+
+  // 2. Called by AuthController after successful LocalStrategy validation
+  async login(user: any) {
+    const payload = { phone: user.phone, sub: user.password };
+
+    return {
+      message: 'login successfull',
+      access_token: this.jwtService.sign(payload),
+    };
+  }
 }
