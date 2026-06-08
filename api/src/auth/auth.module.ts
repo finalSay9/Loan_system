a031c8cad3156660ffca64a5b2d0a336ed9290ea
finalSay9 <chimwazaevan1@gmail.com>
2026-06-08 13:18:06 +0200

    fixed the jwt problem

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,15 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersService } from 'src/users/users.service';
-import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from 'src/users/users.module';
-import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule,PassportModule],
-  providers: [AuthService, LocalStrategy, JwtService],
-  controllers: [AuthController]
+  imports: [
+    ConfigModule,
+    PassportModule,
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: (config.get<string>('JWT_EXPIRES_IN') ?? '7d') as any,
+        },
+      }),
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
