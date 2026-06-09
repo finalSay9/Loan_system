ed253683d71ed989671d609ac3574534f8146b6d
finalSay9 <chimwazaevan1@gmail.com>
2026-06-09 19:18:20 +0200

    creating the guards to handle the requests in the protected routes

--- api/src/auth/guards/auth.guard.ts
+++ api/src/auth/guards/auth.guard.ts
@@ -1,6 +1,13 @@
 import { Injectable, ExecutionContext, CanActivate } from '@nestjs/common';
+import { Observable } from 'rxjs';
 
 @Injectable() 
 export class AuthGuard implements CanActivate {
-    
+    //implementing the can activate function
+    canActivate(
+        context: ExecutionContext,
+    ): Observable<boolean> {
+        const request = context.switchToHttp().getRequest()
+        return validateRequest(request)
+    }
 }
