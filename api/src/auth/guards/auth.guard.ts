0ad37ab861e333f962fd4e465eb8235050e32913
finalSay9 <chimwazaevan1@gmail.com>
2026-06-11 10:02:45 +0200

    still exploring the guards

--- api/src/auth/guards/auth.guard.ts
+++ api/src/auth/guards/auth.guard.ts
@@ -4,9 +4,14 @@ import { Injectable, ExecutionContext, CanActivate } from '@nestjs/common';
 @Injectable()
 export class AuthGuard implements CanActivate {
   //implementing the can activate function
-  canActivate(context: ExecutionContext):boolean | Promise<boolean> {
+  async canActivate(context: ExecutionContext):Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    return validateRequest(request);
+    return await this.validateRequest(request);
+  }
+
+  private async validateRequest(request: any):Promise<boolean> {
+    return true;
+
   }
 }
 
