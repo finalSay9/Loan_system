9ac947d7ee71a302902e04c02c5d55efa99b6072
finalSay9 <chimwazaevan1@gmail.com>
2026-06-15 15:44:50 +0200

    updating the loan dto

--- api/src/loans/dto/create-loan-dto.ts
+++ api/src/loans/dto/create-loan-dto.ts
@@ -1,5 +1,16 @@
-import { IsString, IsNumber } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsDecimal,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
 
 export class CreateLoanDto {
-    
+
 }
