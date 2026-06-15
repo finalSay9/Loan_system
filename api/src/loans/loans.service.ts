3fabec4e8505f4c3a37618759fa2f553d206fcb1
finalSay9 <chimwazaevan1@gmail.com>
2026-06-15 15:24:35 +0200

    now venturing ito the implementaion of the loan service

--- api/src/loans/loans.service.ts
+++ api/src/loans/loans.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class LoansService {}
+export class LoansService {
+
+    constructor(
+        private prisma: PrismaService
+    ){}
+}
