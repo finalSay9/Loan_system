6c87a858c70a5ecd15af30c90436196fd0a28fa9
finalSay9 <chimwazaevan1@gmail.com>
2026-06-17 08:12:51 +0200

    impelementing the loan application endpoint in loans controller

--- api/src/loans/loans.controller.ts
+++ api/src/loans/loans.controller.ts
@@ -1,5 +1,6 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { LoansService } from './loans.service';
+import { CreateLoanDto } from './dto/create-loan-dto';
 
 @Controller('loans')
 export class LoansController {
@@ -7,7 +8,9 @@ export class LoansController {
     constructor(private loanService: LoansService){}
 
     @Post()
-    
+    async createLoan(@Body() loanDto: CreateLoanDto, userId: string) {
+        return await this.loanService.applyForLoan(loanDto, userId)
+    }
 
 
 }
