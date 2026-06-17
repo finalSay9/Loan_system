7afd6ded197766465ca7b176e942f770cae0ac61
finalSay9 <chimwazaevan1@gmail.com>
2026-06-17 08:21:02 +0200

    adding the useful imports

--- api/src/loans/loans.controller.ts
+++ api/src/loans/loans.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { 
+    Body,
+    Controller, 
+    Post,
+    HttpCode,
+    HttpStatus,
+    Get,
+    Param,
+    Query,
+    UseGuards } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
 import { LoansService } from './loans.service';
 import { CreateLoanDto } from './dto/create-loan-dto';
 
@@ -8,6 +18,10 @@ export class LoansController {
     constructor(private loanService: LoansService){}
 
     @Post()
+    @HttpCode(HttpStatus.CREATED)
+    @ApiOperation({ summary: 'Register a new user' })
+    @ApiResponse({ status: 201, description: 'User registered successfully' })
+    @ApiResponse({ status: 409, description: 'Email already in use' })
     async createLoan(@Body() loanDto: CreateLoanDto, userId: string) {
         return await this.loanService.applyForLoan(loanDto, userId)
     }
