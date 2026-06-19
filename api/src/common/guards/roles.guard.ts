4f42d2229ba6bc0f5d89c65397f78c56b98faad6
finalSay9 <chimwazaevan1@gmail.com>
2026-06-19 14:45:59 +0200

    creating the reflectot class and hand;lers

--- api/src/common/guards/roles.guard.ts
+++ api/src/common/guards/roles.guard.ts
@@ -5,18 +5,27 @@ import { Role } from 'prisma/generated/prisma';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
+    //injecting the reflector class(service)
     constructor(private reflector: Reflector){}
 
+    //having the decision maker the canActivate guy
     canActivate(context: ExecutionContext): boolean {
-        const requiredRoles = this.reflector.getAllAndOverride<Role[]>('roles', 
+      //look for metadata key named roles
+      const requiredRoles = this.reflector.getAllAndOverride<Role[]>(
+        'roles',
         //first look at the route/handler if not look at the whole class
-            [context.getHandler(),context.getClass()]);
-        //route is open
-        if(!requiredRoles) {
-            return true
-        }
+        //if its found on both override
+        [context.getHandler(), context.getClass()],
+      );
 
-        const {user} = context.switchToHttp().getRequest();
-        return requiredRoles.includes(user.role)
+      //route is open.....that is ..is the route public
+      if (!requiredRoles) {
+        return true;
+      }
+
+      const { user } = context.switchToHttp().getRequest();
+      //checks if the user's role matches any of the roles written on 
+      // your sticky note/board
+      return requiredRoles.includes(user.role);
     }
 }
