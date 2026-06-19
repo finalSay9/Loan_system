c4cda98b8c5e1dbfe30a95ad980ff492b4eff16a
finalSay9 <chimwazaevan1@gmail.com>
2026-06-19 14:14:16 +0200

    creating the reflector helper service to deal with roles and protected routes

--- api/src/common/guards/roles.guard.ts
+++ api/src/common/guards/roles.guard.ts
@@ -1,1 +1,20 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from 'prisma/generated/prisma';
+
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+    constructor(private reflector: Reflector){}
+
+    canActivate(context: ExecutionContext): boolean {
+        const requireRoles = this.reflector.getAllAndOverride<Role[]>('roles', [
+            context.getHandler(),
+            context.getClass()
+        ]);
+        //route is open
+        if(!requireRoles) {
+            return true
+        }
+    }
+}
